b88a4eef1467f3795f407377553f90a5acdfd281
EstephaniaCalvoC <2177@holbertonschool.com>
2021-02-10 22:02:14 -0500

    Add testing for save and to_dict methods
    Add test case for init and str  methods

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -46,11 +46,18 @@ class TestBaseModel_init(unittest.TestCase):
         b2 = BaseModel()
         self.assertLess(b1.updated_at, b2.updated_at)
 
+    # Testing new attributes creation
+    def test_new_attr(self):
+        b = BaseModel()
+        b.name = "Holberton"
+        b.my_number = 89
+        self.assertTrue(hasattr(b, "name") and hasattr(b, "my_number"))
 
-# class TestBaseModel_str(unittest.TestCase):
-#     """Test __str__ method of BaseModel class"""
 
-    def test_empty_input(self):
+class TestBaseModel_str(unittest.TestCase):
+    """Test __str__ method of BaseModel class"""
+
+    def test_empty_input_str(self):
         b = BaseModel()
         b_str = str(b)
 
@@ -58,8 +65,85 @@ class TestBaseModel_init(unittest.TestCase):
         len_part1 = len(part1) + len(b.id) + 2
         real1 = b_str[: len_part1]
         exp1 = part1 + b.id + ") "
-        self.assertEqual(real1, exp1)
+        self.assertEqual(exp1, real1)
 
         real2 = eval(b_str[len_part1:])
         exp2 = b.__dict__
-        self.assertEqual(real2, exp2)
+        self.assertEqual(exp2, real2)
+
+    def test_new_attr_str(self):
+        b = BaseModel()
+        b.name = "Holberton"
+        b.my_number = 89
+        b_str = str(b)
+
+        part1 = "[BaseModel] ("
+        len_part1 = len(part1) + len(b.id) + 2
+        real1 = b_str[: len_part1]
+        exp1 = part1 + b.id + ") "
+        self.assertEqual(exp1, real1)
+
+        real2 = eval(b_str[len_part1:])
+        exp2 = b.__dict__
+        self.assertEqual(exp2, real2)
+
+
+class TestBaseModel_save(unittest.TestCase):
+    """Test save method of BaseModel class"""
+
+    def test_update_date(self):
+        b = BaseModel()
+        date1 = b.updated_at
+        sleep(0.02)
+        b.save()
+        date2 = b.updated_at
+        self.assertLess(date1, date2)
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """Test to_dict method of BaseModel class"""
+
+    def test_class_item(self):
+        b = BaseModel()
+        b_dict = b.to_dict()
+        real = b_dict["__class__"]
+        exp = "BaseModel"
+
+    def test_created_at_format(self):
+        b = BaseModel()
+        b.created_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)
+        b_dict = b.to_dict()
+        real = b_dict["created_at"]
+        exp = "2017-09-28T21:05:54.119427"
+        self.assertEqual(exp, real)
+
+    def test_update_at_format(self):
+        b = BaseModel()
+        b.updated_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119572)
+        b_dict = b.to_dict()
+        real = b_dict["updated_at"]
+        exp = "2017-09-28T21:05:54.119572"
+        self.assertEqual(exp, real)
+
+    def test_empty_input_format(self):
+        b = BaseModel()
+        b.created_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)
+        b.updated_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119572)
+        real = b.to_dict()
+        exp = {'__class__': 'BaseModel',
+            'updated_at': '2017-09-28T21:05:54.119572',
+            'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579',
+            'created_at': '2017-09-28T21:05:54.119427'}
+
+    def test_new_attr_format(self):
+        b = BaseModel()
+        b.created_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)
+        b.updated_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119572)
+        b.name = "Holberton"
+        b.my_number = "89"
+        real = b.to_dict()
+        exp = {'my_number': 89,
+            'name': 'Holberton',
+            '__class__': 'BaseModel',
+            'updated_at': '2017-09-28T21:05:54.119572',
+            'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579',
+            'created_at': '2017-09-28T21:05:54.119427'}
