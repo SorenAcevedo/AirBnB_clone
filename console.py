40653b508fd13df668a80d1a863ef743691f7548
EstephaniaCalvoC <2177@holbertonschool.com>
2021-02-12 16:44:45 -0500

    Add 'create' command to console -> Task: 7

--- console.py
+++ console.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """Define the HBNBCommand class."""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
+def parse(text_args):
+    """Parse all argurments for the console"""
+    l_args = text_args.split(" ")
+    return l_args[0:]
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter.
@@ -11,6 +17,9 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel": BaseModel
+    }
 
     def do_quit(self, arg):
         """Quit command to exit the program
@@ -27,6 +36,40 @@ class HBNBCommand(cmd.Cmd):
         """Ignore an empty line.
         """
         pass
+
+    # Commands
+
+    def do_create(self, arg):
+        """Usage --> create <class>
+        Create a new instance of the indicated class and:
+            - Saves it (to the JSON file)
+            - Prints the id
+
+        Errors:
+        =======
+        ```
+        (hbnb) create
+        ** class name missing **
+        ```
+
+        ```
+        (hbnb) create MyModel
+        ** class doesn't exist **
+        ```
+        """
+
+        l_args = parse(arg)
+        d_cls = HBNBCommand.__classes
+
+        if len(l_args) == 0:
+            print("** class name missing **")
+        elif l_args[0] not in d_cls.keys():
+            print("** class doesn't exist **")
+        else:
+            key_cls = l_args[0]
+            obj = d_cls[key_cls]()
+            print(obj.id)
+            storage.save()
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
