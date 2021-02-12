d018a9e27ddd701007d5fd3fb6ba7d6e858ee958
SorenAcevedo <sorenacevedo@gmail.com>
2021-02-12 14:31:20 -0500

    Add point 05

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ Base class """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -12,16 +13,19 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for k, v in kwargs.items():
                 if k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                    fmt = "%Y-%m-%dT%H:%M:%S.%f"
+                    setattr(self, k, datetime.strptime(v, fmt))
                 elif k != "__class__":
                     setattr(self, k, v)
 
     def save(self):
         """ Save Method """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Convert to dictionary """
