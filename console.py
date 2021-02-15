192ec5154c67a180c8ec50424c2b960158f3ec28
EstephaniaCalvoC <2177@holbertonschool.com>
2021-02-14 22:49:26 -0500

    Add destroy and all functions to console script - Task 7

--- console.py
+++ console.py
@@ -7,7 +7,6 @@ from models import storage
 def parse(text_args):
     """Parse all argurments for the console"""
     l_args = text_args.split(" ")
-    print(l_args)
     if l_args == ['']:
         return []
     return l_args[0:]
@@ -117,6 +116,77 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(storage.all()[key_obj])
+
+    def do_destroy(self, arg):
+        """Usage --> destroy <class> <id>
+        Deletes an instance based on the class name and id,
+        also update the JSON file.
+
+        Errors:
+        =======
+        ```
+        (hbnb) destroy
+        ** class name missing **
+        ```
+
+        ```
+        (hbnb) destroy MyModel 1234-1234
+        ** class doesn't exist **
+        ```
+
+        ```
+        (hbnb) destroy BaseModel
+        ** instance id missing **
+        ```
+
+        ```
+        (hbnb) destroy BaseModel 12341234
+        ** no instance found **
+        ```
+        """
+
+        l_args = parse(arg)
+        d_cls = HBNBCommand.__classes
+
+        if len(l_args) == 0:
+            print("** class name missing **")
+        elif l_args[0] not in d_cls.keys():
+            print("** class doesn't exist **")
+        elif len(l_args) == 1:
+            print("** instance id missing **")
+        else:
+            key_obj = l_args[0] + "." + l_args[1]
+            if key_obj not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                del storage.all()[key_obj]
+                storage.save()
+
+    def do_all(self, arg):
+        """Usage --> all <class>
+        Prints all string representation of all instances
+        based or not on the class name.
+        Note: <class> is optional.
+
+        Errors:
+        =======
+        ```
+        (hbnb) all MyModel
+        ** class doesn't exist **
+        ```
+        """
+
+        l_args = parse(arg)
+        d_cls = HBNBCommand.__classes
+        val_obj = storage.all().values()
+
+        if len(l_args) == 0:
+            print([str(obj) for obj in val_obj])
+        elif l_args[0] not in d_cls.keys():
+            print("1")
+            print("** class doesn't exist **")
+        else:
+            print([str(obj) for obj in val_obj if obj.__class__.__name__ == l_args[0]])
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
