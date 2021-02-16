7d9a2337c6db43ced1a615c079ab68bdac690a70
SorenAcevedo <sorenacevedo@gmail.com>
2021-02-16 13:53:02 -0500

    Add some commments

--- models/user.py
+++ models/user.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-""" User Class """
+""" User Class Module """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ User Class """
+    """ User Class
+    Public Attributes:
+    -----------------
+    - email
+    - password
+    - first_name
+    - last_name
+    """
     email = ""
     password = ""
     first_name = ""
